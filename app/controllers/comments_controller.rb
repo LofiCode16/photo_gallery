6483949ce568ba4ac15282341f0e2fa70c46225a
Diego Perez <diegopr178@gmail.com>
2021-02-17 16:11:33 -0300

    FIX: comment params syntax error

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -18,6 +18,6 @@ class CommentsController < ApplicationController
 
   private
   def comment_params
-    params.require(:comment),permit(:content, :photo_id)
+    params.require(:comment).permit(:content, :photo_id)
   end
 end
