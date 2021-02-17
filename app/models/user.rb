790418dee4c7ba24bb884bbf2f78cf1c3d79bc48
Diego Perez <diegopr178@gmail.com>
2021-02-17 16:09:25 -0300

    FIX: user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,15 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable #:confirmable
 
-<<<<<<< HEAD
-  has_many :photos      
-
-=======
   has_many :photos    
   has_many :comments
   
   def to_s
     self.nickname
   end
->>>>>>> comments
 end
