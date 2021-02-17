8b846c8c3db399cbe11607934577cb593eb0088d
Diego Perez <diegopr178@gmail.com>
2021-02-17 14:48:57 -0300

    ADD: photos model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'photos/index'
+  get 'photos/:id', to: 'photos#show', as: 'photo'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root to: "home#index"
+  root to: "photos#index"
 end
